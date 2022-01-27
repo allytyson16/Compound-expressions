f78254fc35cf1ab33b386fdb0de6269dbeeea90d
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-01-27 19:18:15 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace Udemyyyyy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter your age - ");
+		    int agge = int.Parse(Console.ReadLine());
+		
+		
+		    if(agge >= 12 && agge <= 55)
+		    {
+		    	Console.WriteLine("You are good to get a job.");
+	    	}
+		
+		    else
+		    {
+			    Console.WriteLine("You are either young or old to get a job");
+		    }        
         }
     }
 }
